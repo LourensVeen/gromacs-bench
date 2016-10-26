1ad03f59077b127581b3e76253d48c3688e21b9d
Lourens Veen <l.veen@esciencecenter.nl>
2016-10-26 17:19:21 +0200

    Add replicas and Ethernet over Infiniband benchmarks

--- run-das5-vu.sh
+++ run-das5-vu.sh
@@ -2,21 +2,29 @@
 
 # Script for running experiments on the VU cluster of DAS-5
 
-# Naming scheme: run_n<nodes>_t<TitanX_per_node>_g<GTX980_per_node>_i<interconnect>
+# Naming scheme: run_n<nodes>_t<TitanX_per_node>_g<GTX980_per_node>_i<interconnect>_r<replica>
 # where <interconnect> is i for Infiniband, e for Ethernet
 
-# CPU only runs with Infiniband
-for NUM_NODES in 1 2 4 8 16 24 ; do
-	sbatch -N $NUM_NODES -C cpunode gromacs-bench.slurm run_n${NUM_NODES}_t0_g0_ii infiniband
-done
+for REPLICA in seq 10 ; do
 
-# CPU+GPU TitanX runs with Infiniband
-for NUM_NODES in 1 2 4 6 8 ; do
-	sbatch -N $NUM_NODES -C TitanX --gres=gpu:1 gromacs-bench.slurm run_n${NUM_NODES}_t1_g0_ii infiniband
-done
+	# CPU only runs with Infiniband
+	for NUM_NODES in 1 2 4 8 16 24 ; do
+		sbatch -N $NUM_NODES -C cpunode gromacs-bench.slurm run_n${NUM_NODES}_t0_g0_ii_r${REPLICA} infiniband
+	done
 
-# CPU only with Ethernet
-for NUM_NODES in 1 2 4 6 8 ; do
-	sbatch -N $NUM_NODES -C cpunode gromacs-bench.slurm run_n${NUM_NODES}_t0_g0_ie ethernet
+	# CPU+GPU TitanX runs with Infiniband
+	for NUM_NODES in 1 2 4 6 8 ; do
+		sbatch -N $NUM_NODES -C TitanX --gres=gpu:1 gromacs-bench.slurm run_n${NUM_NODES}_t1_g0_ii_r${REPLICA} infiniband
+	done
+
+	# CPU only with Ethernet
+	for NUM_NODES in 1 2 4 6 8 ; do
+		sbatch -N $NUM_NODES -C cpunode gromacs-bench.slurm run_n${NUM_NODES}_t0_g0_ie_r${REPLICA} ethernet
+	done
+
+	# CPU only with Ethernet over Infiniband
+	for NUM_NODES in 1 2 4 6 8 ; do
+		sbatch -N $NUM_NODES -C cpunode gromacs-bench.slurm run-n${NUM_NODES}_t0_g0_im_r${REPLICA} ethinf
+	done
 done
 
