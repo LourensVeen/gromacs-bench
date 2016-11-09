8ef3ff2479b333e3317efb5c5857839a516b9334
Lourens Veen <l.veen@esciencecenter.nl>
2016-11-09 11:21:43 +0100

    Add to output which nodes we ran on and GPU info

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'Run name,Num. nodes,Num. threads,Interconnect,Decomposition,Avg. atoms per domain,Min. atoms per domain,Max. atoms per domain,Atoms comm. per step force,Atoms comm. per step vsites,Atoms comm. per step LINCS,Performance (ns/day),Total core time,Wallclock time' >data.csv
+echo 'Run name,Num. nodes,Num. threads,Num. TitanX,Num. GTX980,Interconnect,Decomposition,Avg. atoms per domain,Min. atoms per domain,Max. atoms per domain,Atoms comm. per step force,Atoms comm. per step vsites,Atoms comm. per step LINCS,Performance (ns/day),Total core time,Wallclock time,Which nodes' >data.csv
 
 for RUN_NAME in `ls -1 runs` ; do
 
@@ -8,8 +8,18 @@ for RUN_NAME in `ls -1 runs` ; do
 	METADATA="runs/$RUN_NAME/metadata.txt"
 
 	NUM_NODES=`grep '^Number of nodes: ' ${METADATA} | sed -e 's/^Number of nodes: //'`
+	WHICH_NODES=`grep '^Nodes: ' ${METADATA} | sed -e 's/^Nodes: //'`
 	NUM_THREADS=`grep '^Total threads: ' ${METADATA} | sed -e 's/^Total threads: //'`
 	INTERCONNECT=`grep '^Interconnect: ' ${METADATA} | sed -e 's/^Interconnect: //'`
+
+	NUM_TITANX=0
+	NUM_GTX980=0
+	NUM_GPUS=`grep '^    Number of GPUs detected:' ${LOG} | sed -e 's/^    Number of GPUs detected:[[:space:]]*//'`
+	if grep -q 'NVIDIA GeForce GTX TITAN X' ${LOG} ; then
+		NUM_TITANX="$NUM_GPUS"
+	elif grep -q 'NVIDIA GeForce GTX 980' ${LOG} ; then
+		NUM_GTX980="$NUM_GPUS"
+	fi
 
 	DECOMPOSITION=`grep '^Domain decomposition grid' ${LOG} | sed -e 's/^Domain decomposition grid //' | sed -e 's/, separate PME ranks .*//'`
 
@@ -37,6 +47,8 @@ for RUN_NAME in `ls -1 runs` ; do
 
 	echo -n ",$NUM_NODES" >>data.csv
 	echo -n ",$NUM_THREADS" >>data.csv
+	echo -n ",$NUM_TITANX" >>data.csv
+	echo -n ",$NUM_GTX980" >> data.csv
 	echo -n ",$INTERCONNECT" >>data.csv
 
 	echo -n ",$DECOMPOSITION" >>data.csv
@@ -52,6 +64,8 @@ for RUN_NAME in `ls -1 runs` ; do
 	echo -n ",$PERF_NS_DAY" >>data.csv 
 	echo -n ",$PERF_TOTAL_CORE_TIME" >>data.csv 
 	echo -n ",$PERF_WALL_TIME" >>data.csv
+
+	echo -n ",$WHICH_NODES" >>data.csv
 
 	echo >>data.csv
 done
